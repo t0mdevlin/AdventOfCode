b95050435c05d1f73acdf7bd7a5928089635a60d
Tom Devlin <tomdevlin@Toms-MacBook-Pro.local>
2021-11-25 13:51:35 +0000

    added folders, refactored d1 2015

--- Samples/2015Day2.py
+++ Samples/2015Day2.py
@@ -1,6 +1,9 @@
+#2015 Day 2 Puzzle
+
 result = 0
 f = open('2015Day2Text.txt')
-
+print(f.read())
+"""
 for line in f:
     numbers = []
     for word in a_string.split():
@@ -8,7 +11,7 @@ for line in f:
           numbers.append(int(word))
 
     print(numbers)
-
+"""
 
 
 
